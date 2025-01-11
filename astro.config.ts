8bf650d4fe72cca8c20ccd20a2a0fd929aeebda0
Andreas Schlapbach <schlpbch@gmail.com>
2025-01-11 14:40:19 +0100

    feat: add sitemap integration to Astro configuration and update dependencies

--- astro.config.ts
+++ astro.config.ts
@@ -2,15 +2,14 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: process.env.CI
     ? 'https://kultur-verein-muri-guemligen.netlify.app/'
     : 'http://localhost:4321',
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [react(), tailwind({
+    applyBaseStyles: false,
+  }), sitemap()],
 })
