0f9b95412ce64bd510b99a3356558a0bb0138e3a
Andreas Schlapbach <andreas.schlapbach@sbb.ch>
2025-12-06 16:39:06 +0100

    refactor: improve code quality and fix type errors

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,30 +1,34 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
+import type { CollectionEntry } from 'astro:content'
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export async function filterPastEvents(events) {
+export async function filterPastEvents(events: CollectionEntry<'events'>[]) {
   return events.filter((event) => {
     return new Date(event.data.date) < new Date()
   })
 }
 
-export async function filterFutureEvents(events) {
+export async function filterFutureEvents(events: CollectionEntry<'events'>[]) {
   return events.filter((event) => {
     return new Date(event.data.date) >= new Date()
   })
 }
 
-export async function sortEventsByDate(events) {
+export async function sortEventsByDate(events: CollectionEntry<'events'>[]) {
   return events.sort((a, b) => {
-    return new Date(a.data.date) - new Date(b.data.date)
+    return new Date(a.data.date).getTime() - new Date(b.data.date).getTime()
   })
 }
 
-export async function invertSortEventsByDate(events) {
+export async function invertSortEventsByDate(
+  events: CollectionEntry<'events'>[],
+) {
   return events.sort((a, b) => {
-    return new Date(b.data.date) - new Date(a.data.date)
+    return new Date(b.data.date).getTime() - new Date(a.data.date).getTime()
   })
 }
